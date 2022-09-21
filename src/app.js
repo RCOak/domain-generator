9316fb28eb2c744431f8a842e1534630f0b5e0ca
RCOak <coliveirarenato90@gmail.com>
2022-09-21 19:10:20 +0000

    att

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+pronoun.forEach(function(elemento) {
+  adj.forEach(function(elemento1) {
+    noun.forEach(function(elemento2) {
+      dominio.forEach(function(elemento3) {
+        console.log(elemento + elemento1 + elemento2 + elemento3);
+      });
+    });
+  });
+});
+// for(let elemento of pronoun){
+//     for(let elemento1 of adj)
+//     {
+//         for(let elemento2 of noun)
+//             {for(let elemento3 of dominio)
+//                 {
+//                 console.log(elemento+ elemento1 + elemento2 + elemento3);
+//                 };
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//         };
+//     };
+// };
